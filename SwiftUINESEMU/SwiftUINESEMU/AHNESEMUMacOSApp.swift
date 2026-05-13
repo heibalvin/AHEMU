4344a2df22cf20c5a78213e262126cb817d52844
Alvin HEIB <heibalvin@gmail.com>
2026-05-13 10:34:37 +0400

    removed NESEMU swift package, and merged with AHNESEMUMacOS app

---
+++ SwiftUINESEMU/SwiftUINESEMU/AHNESEMUMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AHNESEMUMacOSApp.swift
+//  AHNESEMUMacOS
+//
+//  Created by Alvin HEIB on 12/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AHNESEMUMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NESEMUView()
+        }
+    }
+}
---
+++ SwiftUINESEMU/SwiftUINESEMU/NESPPUView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import SpriteKit
+
+struct NESPPUView: View {
+    @StateObject private var scene: NESRenderScene
+
+    init(emu: NESEMU) {
+        _scene = StateObject(wrappedValue: NESRenderScene(ppu: emu.ppu))
+    }
+
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            SpriteView(scene: scene)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack(alignment: .trailing, spacing: 4) {
+                Text("FPS: \(scene.fps, specifier: "%.0f")")
+                    .font(.caption)
+                    .monospacedDigit()
+                    .foregroundColor(.white)
+            }
+            .padding(.top, 8)
+            .padding(.trailing, 8)
+        }
+        .onAppear { scene.startFPSTimer() }
+        .onDisappear { scene.stopFPSTimer() }
+    }
+}
